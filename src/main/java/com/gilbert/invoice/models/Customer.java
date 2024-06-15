1c173348d8a74eb5b56706fd5e17341389a35d45
Gilbert Baracka <gilbert.baracka@gmail.com>
2024-06-15 16:51:49 -0600

    set up folder structures

---
+++ src/main/java/com/gilbert/invoice/models/Customer.java
@@ -0,0 +1,44 @@
+package com.gilbert.invoice.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.Target;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(
+        name = "customer",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "email_unique",
+                        columnNames = "email"
+                )
+        }
+)
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long customerId;
+    @Column(nullable = false)
+    private String customerName;
+    @Column(nullable = false)
+    private String email;
+    private String address;
+    private String city;
+    private String state;
+    @Column(length = 10)
+    private String zipCode;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
